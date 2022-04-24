32be6a24a85414094f37c06620532de63620800e
Chetna <69139323+chetna14manku@users.noreply.github.com>
2022-04-24 12:32:15 +0530

    Update aws.py

--- aws.py
+++ aws.py
@@ -1,73 +1,72 @@
 import os
 import getpass
 
-print("\t\t\tWelcome to the TUI to make Life Easier")
+def main():
+    print("\t\t\tWelcome to the TUI to make Life Easier")
 
-print("\t\t\t--------------------------------------")
-
-
-
-print("\t\t\t\t\tMenu")
-print("\t\t\t\t\t----")
-print("""
- Press  1 : To Login into AWS CLI
- Press  2 : To Launch a instance
- Press  3 : To Start a Instance
- Press  4 : To Stop a Instance 
- Press  5 : To Describe All Instances 
- Press  6 : Exit
-
-""")
-
-print("Enter Your Choice : ",end="")
-
-ch=input()
+    print("\t\t\t--------------------------------------")
 
 
-if int(ch) == 1:
-
-    os.system("aws configure")
-
-elif int(ch) == 2:
+    print("\t\t\t\t\tMenu")
+    print("\t\t\t\t\t----")
     print("""
+     Press  1 : To Login into AWS CLI
+     Press  2 : To Launch a instance
+     Press  3 : To Start a Instance
+     Press  4 : To Stop a Instance 
+     Press  5 : To Describe All Instances 
+     Press  6 : Exit
+    """)
+
+    print("Enter Your Choice : ",end="")
+
+    ch=input()
+
+    if int(ch) == 1:
+
+        os.system("aws configure")
+
+    elif int(ch) == 2:
+        print("""
             Press 1:AWS Linux
             Press 2:Redhat Linux
             """)
-    print("Enter your Choice :  ",end="")
-    img=input()
-    if int(img) ==1:
-        print("Enter Your Key name: ",end ="")
-        key = input()
-        os.system("aws ec2 run-instances --image-id ami-0f9fc25dd2506cf6d --subnet-id subnet-02c8d39202cd28ae7 --instance-type t2.micro --key-name {} --security-group-ids sg-0cfb581ea9318d7f3 --count 1".format(key))
-    elif int(img) == 2:
-        print("Enter Your Key name: ",end ="")
-        key = input()
-        os.system("aws ec2 run-instances --image-id ami-0b0af3577fe5e3532 --subnet-id subnet-02c8d39202cd28ae7 --instance-type t2.micro --key-name {} --security-group-ids sg-0cfb581ea9318d7f3 --count 1".format(key))
+        print("Enter your Choice :  ",end="")
+        img=input()
+        if int(img) ==1:
+            print("Enter Your Key name: ",end ="")
+            key = input()
+            os.system("aws ec2 run-instances --image-id ami-0f9fc25dd2506cf6d --subnet-id subnet-02c8d39202cd28ae7 --instance-type t2.micro --key-name {} --security-group-ids sg-0cfb581ea9318d7f3 --count 1".format(key))
+        elif int(img) == 2:
+            print("Enter Your Key name: ",end ="")
+            key = input()
+            os.system("aws ec2 run-instances --image-id ami-0b0af3577fe5e3532 --subnet-id subnet-02c8d39202cd28ae7 --instance-type t2.micro --key-name {} --security-group-ids sg-0cfb581ea9318d7f3 --count 1".format(key))
 
 
-elif int(ch) == 3:
+    elif int(ch) == 3:
 
-    print("Enter Instance Id : ",end = "")
-    uid = input()
-    os.system(" aws ec2 start-instances --instance-id {}".format(uid))
+        print("Enter Instance Id : ",end = "")
+        uid = input()
+        os.system(" aws ec2 start-instances --instance-id {}".format(uid))
 
 
-elif int(ch) == 4:
+    elif int(ch) == 4:
     
-    print("Enter Instance Id : ",end = "")
-    uid = input()
-    os.system(" aws ec2 stop-instances --instance-id {}".format(uid))
+        print("Enter Instance Id : ",end = "")
+        uid = input()
+        os.system(" aws ec2 stop-instances --instance-id {}".format(uid))
 
 
-elif int(ch) == 5:
+    elif int(ch) == 5:
 
-    os.system("aws ec2 describe-instances")
+        os.system("aws ec2 describe-instances")
 
-elif int(ch) == 6:
-    exit()
+
+    elif int(ch) == 6:
+        exit()
     
-else:
-    print("You Entered Wrong Choice ...")
-© 2021 GitHub, Inc.
-Terms
-Privacy
+    else:
+        print("You Entered Wrong Choice ...")
+
+if __name__=='__main__':
+        main()    
